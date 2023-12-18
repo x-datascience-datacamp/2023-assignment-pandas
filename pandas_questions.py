c545d3bc5c01aad1baa15090bac84c3e7b952576
Marius Ortega <ortegasaurelmarius@gmail.com>
2023-12-18 16:40:26 +0100

    Up my solution

--- pandas_questions.py
+++ pandas_questions.py
@@ -24,10 +24,10 @@ def load_data():
 
 def merge_regions_and_departments(regions, departments):
     """Merge regions and departments in one DataFrame.
+
     The columns in the final DataFrame should be:
     ['code_reg', 'name_reg', 'code_dep', 'name_dep']
     """
-
     regions_and_departments = regions.merge(
         departments,
         left_on="code",
@@ -43,10 +43,10 @@ def merge_regions_and_departments(regions, departments):
 
 def merge_referendum_and_areas(referendum, regions_and_departments):
     """Merge referendum and regions_and_departments in one DataFrame.
+
     You can drop the lines relative to DOM-TOM-COM departments, and the
     french living abroad.
     """
-
     referendum = referendum[~referendum["Department code"]
                             .str.contains("Z")].copy()
     referendum["Department code"] = referendum["Department code"].str.zfill(2)
@@ -61,6 +61,7 @@ def merge_referendum_and_areas(referendum, regions_and_departments):
 
 def compute_referendum_result_by_regions(referendum_and_areas):
     """Return a table with the absolute count for each region.
+
     The return DataFrame should be indexed by `code_reg` and have columns:
     ['name_reg', 'Registered', 'Abstentions', 'Null', 'Choice A', 'Choice B']
     """
@@ -73,13 +74,13 @@ def compute_referendum_result_by_regions(referendum_and_areas):
 
 def plot_referendum_map(referendum_result_by_regions):
     """Plot a map with the results from the referendum.
+
     * Load the geographic data with geopandas from `regions.geojson`.
     * Merge these info into `referendum_result_by_regions`.
     * Use the method `GeoDataFrame.plot` to display the result map. The results
       should display the rate of 'Choice A' over all expressed ballots.
     * Return a gpd.GeoDataFrame with a column 'ratio' containing the results.
     """
-
     regions = gpd.read_file("data/regions.geojson")
     referendum_result_by_regions = regions.merge(
         referendum_result_by_regions,
