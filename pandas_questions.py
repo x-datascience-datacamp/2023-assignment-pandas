4e30b24b71ca9ba7e788342b3eae7e96f08b5fec
AlexiaALLAL <alexia.allal@etu.minesparis.psl.eu>
2023-12-18 17:20:30 +0100

    Revert "UP my solution"
    This reverts commit 495a2227b177a23a531c4b0557ca86884daa366a.

--- pandas_questions.py
+++ pandas_questions.py
@@ -15,9 +15,10 @@ import matplotlib.pyplot as plt
 
 def load_data():
     """Load data from the CSV files referundum/regions/departments."""
-    referendum = pd.read_csv("data/referendum.csv", sep=";")
-    regions = pd.read_csv("data/regions.csv", sep=",")
-    departments = pd.read_csv("data/departments.csv", sep=",")
+    referendum = pd.DataFrame({})
+    regions = pd.DataFrame({})
+    departments = pd.DataFrame({})
+
     return referendum, regions, departments
 
 
@@ -27,17 +28,8 @@ def merge_regions_and_departments(regions, departments):
     The columns in the final DataFrame should be:
     ['code_reg', 'name_reg', 'code_dep', 'name_dep']
     """
-    regions = regions.copy()
-    departments = departments.copy()
-    regions = regions.rename(columns={'code': 'code_reg', 'name': 'name_reg'})
-    departments = departments.rename(
-        columns={'code': 'code_dep', 'name': 'name_dep'}
-    )
-    df = pd.merge(
-        regions, departments, right_on='region_code', left_on='code_reg'
-    )
-    df = df[['code_reg', 'name_reg', 'code_dep', 'name_dep']]
-    return df
+
+    return pd.DataFrame({})
 
 
 def merge_referendum_and_areas(referendum, regions_and_departments):
@@ -46,16 +38,8 @@ def merge_referendum_and_areas(referendum, regions_and_departments):
     You can drop the lines relative to DOM-TOM-COM departments, and the
     french living abroad.
     """
-    referendum = referendum.copy()
-    regions_and_departments = regions_and_departments.copy()
-    referendum['Department code'] = (
-        referendum['Department code'].astype(str).str.zfill(2)
-        )
-    df = pd.merge(
-        referendum, regions_and_departments,
-        right_on='code_dep', left_on='Department code'
-        )
-    return df
+
+    return pd.DataFrame({})
 
 
 def compute_referendum_result_by_regions(referendum_and_areas):
@@ -64,21 +48,8 @@ def compute_referendum_result_by_regions(referendum_and_areas):
     The return DataFrame should be indexed by `code_reg` and have columns:
     ['name_reg', 'Registered', 'Abstentions', 'Null', 'Choice A', 'Choice B']
     """
-    referendum_and_areas = referendum_and_areas.copy()
-    name_reg = referendum_and_areas[
-        ['code_reg', 'name_reg']
-        ].groupby(['code_reg']).min()
-    referendum_and_areas = referendum_and_areas.drop(
-        columns=['name_reg']
-        ).groupby(['code_reg']).sum()
-    referendum_and_areas = pd.merge(
-        name_reg, referendum_and_areas, on='code_reg'
-        )
-    referendum_and_areas = referendum_and_areas[
-        ['name_reg', 'Registered', 'Abstentions',
-         'Null', 'Choice A', 'Choice B']
-    ]
-    return referendum_and_areas
+
+    return pd.DataFrame({})
 
 
 def plot_referendum_map(referendum_result_by_regions):
@@ -90,26 +61,8 @@ def plot_referendum_map(referendum_result_by_regions):
       should display the rate of 'Choice A' over all expressed ballots.
     * Return a gpd.GeoDataFrame with a column 'ratio' containing the results.
     """
-    geo_regions = gpd.read_file('data/regions.geojson')
-    referendum_result_by_regions = referendum_result_by_regions.copy()
-    referendum_result_by_regions = pd.merge(
-        geo_regions, referendum_result_by_regions,
-        left_on='code', right_on='code_reg'
-        )
 
-    # create a column with the ratio of 'Choice A' over all expressed ballots
-    referendum_result_by_regions['ratio'] = (
-      referendum_result_by_regions['Choice A'] / (
-        referendum_result_by_regions['Choice A']
-        + referendum_result_by_regions['Choice B']
-      )
-    )
-    # display the result map
-    fig, ax = plt.subplots(1, figsize=(10, 6))
-    referendum_result_by_regions.plot(ax=ax, column='ratio', legend=True)
-    ax.set_title('Ratio of "Choice A" over all expressed ballots')
-
-    return referendum_result_by_regions
+    return gpd.GeoDataFrame({})
 
 
 if __name__ == "__main__":
