504b4b6482e50a064b1c44bacf0c0fa8ba4a84ff
Louis TEILLET <lh.teillet@gmail.com>
2023-12-18 16:17:52 +0100

    16h14

--- pandas_questions.py
+++ pandas_questions.py
@@ -15,8 +15,7 @@ import os
 
 
 def find(name, path):
-    """
-    Find a file in a directory."""
+    """Find a file in a directory."""
     for root, dirs, files in os.walk(path):
         if name in files:
             return os.path.join(root, name)
