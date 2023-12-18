c01b0c2f6129fcdbd1b12b63f1a6e9655cccba35
AugustinCreusillet <augustin.creusillet@gmail.com>
2023-12-18 19:33:14 +0100

    MY SOLUTION

--- pandas_questions.py
+++ pandas_questions.py
@@ -71,7 +71,6 @@ def compute_referendum_result_by_regions(referendum_and_areas):
     The return DataFrame should be indexed by `code_reg` and have columns:
     ['name_reg', 'Registered', 'Abstentions', 'Null', 'Choice A', 'Choice B']
     """
-
     referendum_result_by_regions = referendum_and_areas.groupby(
         ['code_reg', 'name_reg']).sum()
     referendum_result_by_regions.reset_index(inplace=True)
