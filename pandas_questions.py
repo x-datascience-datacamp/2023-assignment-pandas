20be086919a862606a68f145ea8515678811825c
cynthiaobeid <cynthia.obeid@telecom-paris.fr>
2023-12-20 15:10:09 +0100

    UP my solution

--- pandas_questions.py
+++ pandas_questions.py
@@ -8,6 +8,7 @@ https://github.com/x-datascience-datacamp/datacamp-assignment-pandas/blob/main/e
 To do that, you will load the data as pandas.DataFrame, merge the info and
 aggregate them by regions and finally plot them on a map using `geopandas`.
 """
+
 import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
@@ -28,7 +29,6 @@ def merge_regions_and_departments(regions, departments):
     The columns in the final DataFrame should be:
     ['code_reg', 'name_reg', 'code_dep', 'name_dep']
     """
-
     return regions.merge(departments, left_on='code',
                          right_on='region_code',
                          suffixes=('_reg', '_dep'))[['code_reg',
@@ -42,7 +42,6 @@ def merge_referendum_and_areas(referendum, regions_and_departments):
     You can drop the lines relative to DOM-TOM-COM departments, and the
     french living abroad.
     """
-
     referendum['Department code'] = referendum['Department code'].apply(
         lambda x: '0' + x if len(x) == 1 else x)
     referendum = referendum[~referendum['Department code'].str.startswith('Z')]
@@ -59,7 +58,6 @@ def compute_referendum_result_by_regions(referendum_and_areas):
     The return DataFrame should be indexed by `code_reg` and have columns:
     ['name_reg', 'Registered', 'Abstentions', 'Null', 'Choice A', 'Choice B']
     """
-
     tmp = referendum_and_areas.groupby(['code_reg', 'name_reg'],
                                        as_index=False)[['Registered',
                                                         'Abstentions', 'Null',
@@ -78,7 +76,6 @@ def plot_referendum_map(referendum_result_by_regions):
       should display the rate of 'Choice A' over all expressed ballots.
     * Return a gpd.GeoDataFrame with a column 'ratio' containing the results.
     """
-
     geo_data = gpd.read_file('data/regions.geojson')
 
     merged_data = referendum_result_by_regions.merge(geo_data,
