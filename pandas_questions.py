727f32bd3d2980635d6ecfd575e31f6f3017cc1d
Pierre Derathe <pierre.derathe@u-psud.fr>
2023-12-20 12:16:42 +0100

    Correction code style

--- pandas_questions.py
+++ pandas_questions.py
@@ -54,7 +54,6 @@ def compute_referendum_result_by_regions(referendum_and_areas):
     The return DataFrame should be indexed by `code_reg` and have columns:
     ['name_reg', 'Registered', 'Abstentions', 'Null', 'Choice A', 'Choice B']
     """
-
     columns = ['code_reg', 'name_reg', 'Registered', 'Abstentions',
                'Null', 'Choice A', 'Choice B']
     col_fun = {'name_reg': 'max', 'Registered': 'sum', 'Abstentions': 'sum',
