8f0490ec7d58848243f49b8c7af02a9f9b325a3f
marounAB <maroun.abouboutros1@gmail.com>
2023-12-20 15:16:12 +0100

    commit 3

--- pandas_questions.py
+++ pandas_questions.py
@@ -28,7 +28,6 @@ def merge_regions_and_departments(regions, departments):
     The columns in the final DataFrame should be:
     ['code_reg', 'name_reg', 'code_dep', 'name_dep']
     """
-
     return regions.merge(
         departments,
         left_on='code',
