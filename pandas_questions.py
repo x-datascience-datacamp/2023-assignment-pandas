80d24342ad7501dc85dcf84af165c8d83301f713
ines-ae <amicheines6@gmail.com>
2023-12-20 17:50:31 +0100

    commit 5

--- pandas_questions.py
+++ pandas_questions.py
@@ -64,7 +64,6 @@ def compute_referendum_result_by_regions(referendum_and_areas):
     The return DataFrame should be indexed by `code_reg` and have columns:
     ['name_reg', 'Registered', 'Abstentions', 'Null', 'Choice A', 'Choice B']
     """
-
     # Drop NaN values (assuming this is the intention)
     referendum_and_areas = referendum_and_areas.dropna()
 
