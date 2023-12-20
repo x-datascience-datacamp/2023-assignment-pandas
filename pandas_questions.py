de7ee404964063fbb63ad433eb6e9c531c6c3fbf
Juliette Danel <juliette.danel@gmail.com>
2023-12-20 15:10:49 +0100

    Erase blank spaces after docstrings

--- pandas_questions.py
+++ pandas_questions.py
@@ -28,7 +28,6 @@ def merge_regions_and_departments(regions, departments):
     The columns in the final DataFrame should be:
     ['code_reg', 'name_reg', 'code_dep', 'name_dep']
     """
-
     departments_renamed = (
         departments.rename(columns={"region_code": "code_reg",
                                     "code": "code_dep",
@@ -55,7 +54,6 @@ def merge_referendum_and_areas(referendum, regions_and_departments):
     You can drop the lines relative to DOM-TOM-COM departments, and the
     french living abroad.
     """
-
     lines_to_drop = referendum.loc[referendum['Department code']
                                    .str
                                    .startswith('Z')]
@@ -81,7 +79,6 @@ def compute_referendum_result_by_regions(referendum_and_areas):
     The return DataFrame should be indexed by `code_reg` and have columns:
     ['name_reg', 'Registered', 'Abstentions', 'Null', 'Choice A', 'Choice B']
     """
-
     referendum_results_by_region = (
         referendum_and_areas
         .groupby(by=["code_reg", "name_reg"], as_index=False)
@@ -106,7 +103,6 @@ def plot_referendum_map(referendum_result_by_regions):
       should display the rate of 'Choice A' over all expressed ballots.
     * Return a gpd.GeoDataFrame with a column 'ratio' containing the results.
     """
-
     geo_regions = gpd.read_file("data/regions.geojson")
 
     gdf_referendum = (
