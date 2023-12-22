9a2927691e84f616276aed486589caf2aef8dd9e
Thomas Savini <tsavini2001@gmail.com>
2023-12-22 17:13:21 +0100

    Formated for flake8

--- pandas_questions.py
+++ pandas_questions.py
@@ -67,7 +67,8 @@ def compute_referendum_result_by_regions(referendum_and_areas):
     The return DataFrame should be indexed by `code_reg` and have columns:
     ['name_reg', 'Registered', 'Abstentions', 'Null', 'Choice A', 'Choice B']
     """
-    aggregated_data = referendum_and_areas.groupby(['code_reg', 'name_reg'], as_index = False).sum()
+    aggregated_data = referendum_and_areas\
+        .groupby(['code_reg', 'name_reg'], as_index=False).sum()
     aggregated_data = aggregated_data.set_index('code_reg')
     print(aggregated_data.shape)
     # Reorder columns as specified
