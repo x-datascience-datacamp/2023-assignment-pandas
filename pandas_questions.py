c08e4fd07e7deecbc7718e55034b56235cbbdcff
Lauriane Mousset <lauriane.mousset@universite-paris-saclay.fr>
2023-12-22 11:33:53 +0100

    assignment pandas

--- pandas_questions.py
+++ pandas_questions.py
@@ -18,6 +18,9 @@ def load_data():
     referendum = pd.DataFrame({})
     regions = pd.DataFrame({})
     departments = pd.DataFrame({})
+    referendum = pd.read_csv("./data/referendum.csv", sep=";")
+    regions = pd.read_csv("./data/regions.csv", sep=",")
+    departments = pd.read_csv("./data/departments.csv", sep=",")
 
     return referendum, regions, departments
 
@@ -28,8 +31,15 @@ def merge_regions_and_departments(regions, departments):
     The columns in the final DataFrame should be:
     ['code_reg', 'name_reg', 'code_dep', 'name_dep']
     """
+    res = pd.merge(
+        left=regions,
+        right=departments,
+        left_on="code",
+        right_on="region_code",
+        suffixes=("_reg", "_dep"),
+    )[["code_reg", "name_reg", "code_dep", "name_dep"]]
 
-    return pd.DataFrame({})
+    return res
 
 
 def merge_referendum_and_areas(referendum, regions_and_departments):
@@ -38,8 +48,25 @@ def merge_referendum_and_areas(referendum, regions_and_departments):
     You can drop the lines relative to DOM-TOM-COM departments, and the
     french living abroad.
     """
+    fr_out = ["ZA", "ZB", "ZC", "ZD", "ZM", "ZN", "ZP", "ZS", "ZW", "ZX", "ZZ"]
 
-    return pd.DataFrame({})
+    # Créer un masque booléen pour les lignes à conserver
+    mask = ~referendum["Department code"].isin(fr_out)
+
+    # Sélectionner les lignes à conserver
+    referendum_filtered = referendum[mask]
+    referendum_filtered["Department code"] = referendum_filtered[
+        "Department code"
+    ].apply(lambda x: "{0:0>2}".format(x))
+
+    ref_reg_dep = pd.merge(
+        left=referendum_filtered,
+        right=regions_and_departments,
+        left_on="Department code",
+        right_on="code_dep",
+    )
+
+    return ref_reg_dep.dropna()
 
 
 def compute_referendum_result_by_regions(referendum_and_areas):
@@ -48,8 +75,19 @@ def compute_referendum_result_by_regions(referendum_and_areas):
     The return DataFrame should be indexed by `code_reg` and have columns:
     ['name_reg', 'Registered', 'Abstentions', 'Null', 'Choice A', 'Choice B']
     """
+    referendum_and_areas_copy = referendum_and_areas.copy()
+    data = referendum_and_areas_copy.groupby("code_reg", as_index=True).agg(
+        {
+            "name_reg": "min",
+            "Registered": "sum",
+            "Abstentions": "sum",
+            "Null": "sum",
+            "Choice A": "sum",
+            "Choice B": "sum",
+        }
+    )
 
-    return pd.DataFrame({})
+    return data
 
 
 def plot_referendum_map(referendum_result_by_regions):
@@ -61,23 +99,29 @@ def plot_referendum_map(referendum_result_by_regions):
       should display the rate of 'Choice A' over all expressed ballots.
     * Return a gpd.GeoDataFrame with a column 'ratio' containing the results.
     """
+    carte_regions = gpd.read_file("./data/regions.geojson")
+    df_tot = pd.merge(
+        left=carte_regions,
+        right=referendum_result_by_regions,
+        left_on="nom",
+        right_on="name_reg",
+    )
+    a = df_tot["Choice A"]
+    b = df_tot["Choice B"]
+    df_tot["ratio"] = a / (a + b)
+    df_tot.plot(column="ratio", cmap="coolwarm", legend=True)
 
-    return gpd.GeoDataFrame({})
+    return df_tot
 
 
 if __name__ == "__main__":
-
     referendum, df_reg, df_dep = load_data()
-    regions_and_departments = merge_regions_and_departments(
-        df_reg, df_dep
-    )
+    regions_and_departments = merge_regions_and_departments(df_reg, df_dep)
     referendum_and_areas = merge_referendum_and_areas(
         referendum, regions_and_departments
     )
-    referendum_results = compute_referendum_result_by_regions(
-        referendum_and_areas
-    )
-    print(referendum_results)
+    ref_results = compute_referendum_result_by_regions(referendum_and_areas)
+    print(ref_results)
 
-    plot_referendum_map(referendum_results)
+    plot_referendum_map(ref_results)
     plt.show()
